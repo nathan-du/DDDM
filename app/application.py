af3e5719abfbb9fef2f54ce0c0e5f39b5987610b
Trevor <trgasdas@ncsu.edu>
2017-04-02 21:51:53 -0400

    Fixed formatting and type bug

--- app/application.py
+++ app/application.py
@@ -6,13 +6,13 @@ app = Flask(__name__)
 @app.route('/', methods = ['GET', 'POST'])
 def check():
     if request.method == 'GET':
-        return "THIS IS THE HOME PAGE"
+        return "THIS IS THE HOME PAGE\n"
     if request.method == 'POST':
         data = request.form
         truth = do_check(data)
-        return "THIS IS THE RESULTS PAGE: " + truth
+        return "THIS IS THE RESULTS PAGE: " + str(truth) + "\n"
     else:
-        return "ERROR METHOD NOT ALLOWED"    
+        return "ERROR METHOD NOT ALLOWED\n"    
 
 def do_check(statement):
     #DO PROCESSING
