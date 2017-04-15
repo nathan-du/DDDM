8b263e28101f31f953165fe0fefb1cde715978cf
Trevor <trgasdas@ncsu.edu>
2017-04-15 00:12:26 -0400

    Added more rules to the immigration file

--- app/immigrationpercountry.py
+++ app/immigrationpercountry.py
@@ -10,6 +10,7 @@ def parseSentence(sentence):
   	# Parse year
 	regexp_year = re.compile('(in|year)\s(\d{4})')
 	m = regexp_year.search(sentence)
+	year = None
 	if m is not None:
 		year_string = m.group()
 		w = year_string.split(' ')
@@ -17,8 +18,9 @@ def parseSentence(sentence):
 		# Replace it with empty string
 		sentence = regexp_year.sub('', sentence)
   
-	regexp_immigrants = re.compile('\d+\s*(people|immigrants|new)')
+	regexp_immigrants = re.compile('\d+')
 	m = regexp_immigrants.search(sentence)
+	immigrants = None
 	if m is not None:
 		immigrants_string = m.group()
 		w = immigrants_string.split(' ')
@@ -32,12 +34,106 @@ def parseSentence(sentence):
 	stop = set(stopwords.words('english'))
 	tokens_selected = [x for x in sentence_tokens if x.lower() not in stop]
 	
-	print(tokens_selected)
 	countries = getCountries(tokens_selected)
-	print(countries)	
+
+	greater = ['greater', 'more', 'at least']
+	less = ['less', 'no more', 'at most']
+
+	less_list = list(map(lambda x: x in sentence.lower(), less))
+	less_result = True in less_list
+	greater_list = list(map(lambda x: x in sentence.lower(), greater))
+	greater_result = True in greater_list
+	
 	if len(countries) > 0:
 		rows = []
 		for country in countries:
-			row = querydb("SELECT * FROM immigrationpercountry WHERE LOWER(Country)= "+country)
+			row = querydb("SELECT * FROM immigrationpercountry WHERE LOWER(country) LIKE '%"+country+"%'")
 			rows.extend(row)
-		print(rows)
+		if year is not None and immigrants is not None:
+			if year < 2003 or year > 2015:
+				return None
+			else:
+				for r in rows:
+					immigrants_actual = r[(year%2000)-1]
+					if immigrants_actual is None:
+						continue
+					immigrants_actual = float(immigrants_actual)
+					if less_result:
+						return immigrants_actual < immigrants
+					elif greater_result:
+						return immigrants_actual > immigrants
+					else:
+						return  immigrants > immigrants_actual*0.98 and immigrants < immigrants_actual*1.02
+				return False
+		elif immigrants is not None:
+			for r in rows:
+				r_copy = list(r)
+				del r_copy[:2]
+				for val in r_copy:
+					if less_result and val < immigrants:
+						return True
+					elif greater_result and val > immigrants:
+						return True
+					elif immigrants > float(val)*0.98 and immigrants < float(val)*1.02:
+						return True
+				total_immigrants = sum(r_copy)
+				if less_result and total_immigrants < immigrants:
+					return True
+				elif greater_result and total_immigrants > immigrants:
+					return True
+				elif immigrants > total_immigrants*0.98 and immigrants < total_immigrants*1.02:
+					return True
+				else:
+					return False
+	else:
+		rows = querydb("SELECT * FROM immigrationpercountry")
+		if year is not None and immigrants is not None:
+			if year < 2003 or year > 2015:
+				return None
+			total_year = 0
+			for row in rows:
+				immigrants_actual = row[(year%2000)-1]
+				if immigrants_actual is None:
+					continue
+				immigrants_actual = float(immigrants_actual)
+				if less_result and immigrants_actual < immigrants:
+					return True
+				if greater_result and immigrants_actual > immigrants:
+					return True
+				if immigrants_actual*0.98 <= immigrants <= 1.02*immigrants_actual:
+					return True
+				total_year += immigrants_actual
+			if less_result and total_year < immigrants:
+				return True
+			if greater_result and total_year > immigrants:
+				return True
+			return immigrants > total_year*0.98 and immigrants < total_year*1.02
+
+		if immigrants is not None:
+			total_immigrants = 0
+			for row in rows:
+				total_country = 0
+				row_copy = list(row)
+				del row_copy[:2]
+				for val in row_copy:
+					if val is None:
+						continue
+					if less_result and val < immigrants:
+						return True
+					if greater_result and val > immigrants:
+						return True
+					if val*0.98 <= immigrants <= val*1.02:
+						return True
+					total_country += val
+				if less_result and total_country < immigrants:
+					return True
+				if greater_result and total_country > immigrants:
+					return True
+				if total_country*0.98 <= immigrants <= total_country*1.02:
+					return True
+				total_immigrants += total_country
+			if less_result and total_immigrants < immigrants:
+				return True
+			if greater_result and total_immigrants > immigrants:
+				return True
+			return total_immigrants*0.98 <= immigrants <= 1.02*total_immigrants 
--- app/testimmigration.py
+++ app/testimmigration.py
@@ -1,5 +1,65 @@
 from immigrationpercountry import parseSentence as ps
 
 s1 = "Number of immigrants from India in the year 2016 was 10000"
+s2 = "Number of immigrants from Gabon in 2003 was 40" 
+s3 = "Number of immigrants from Iran was 7230"
+s4 = "Number of immigrants from Iran was 1"
+s5 = "Total number of immigrants from Iran is 167875"
+s6 = "Number of immigrants from Iran in 2003 was more than 6000"
+s7 = "Number of immigrants from Iran in 2003 was more than 900000"
+s8 = "Number of immigrants from Iran in 2003 was less than 9000000"
+s9 = "Number of immigrants from Iran in 2003 was less than 6000"
+s10 = "More than 10 people have immigrated from Iran"
+s11 = "More than 1000000000 people have immigrated from Iran"
+s12 = "Less than 8000 people have immigrated from Iran"
+s13 = "Less than 12 people have immigrated from Iran"
+s14 = "1252 people immigrated in 2003"
+s15 = "0 people immigrated in 2003"
+s16 = "703521 people immigrated in 2003"
+s17 = "more than 1000 people immigrated in 2003"
+s18 = "more than 700000 people immigrated in 2003"
+s19 = "more than 1000000 people immigrated in 2003"
+s20 = "less than 35 people immigrated in 2003"
+s21 = "less than 1000000 people immigrated in 2003"
+s22 = "less than 0 people immigrated in 2003"
+s23 = "Less than 70 people immigrated"
+s24 = "more than 1000 people immigrated"
+s25 = "1252 people immigrated"
+s26 = "more than 44000 people immigrated"
+s27 = "45619 people immigrated"
+s28 = "more than 13000000 people immigrated"
+s29 = "13534341 people immigrated"
+s30 = "15000000 people immigrated"
 
-print(ps(s1))
+
+
+print(str(1), ps(s1), " - Expected: None")
+print(str(2), ps(s2), " - Expected: True")
+print(str(3), ps(s3), " - Expected: True")
+print(str(4), ps(s4), " - Expected: False")
+print(str(5), ps(s5), " - Expected: True")
+print(str(6), ps(s6), " - Expected: True")
+print(str(7), ps(s7), " - Expected: False")
+print(str(8), ps(s8), " - Expected: True")
+print(str(9), ps(s9), " - Expected: False")
+print(str(10), ps(s10), " - Expected: True")
+print(str(11), ps(s11), " - Expected: False")
+print(str(12), ps(s12), " - Expected: True")
+print(str(13), ps(s13), " - Expected: False")
+print(str(14), ps(s14), " - Expected: True")
+print(str(15), ps(s15), " - Expected: False")
+print(str(16), ps(s16), " - Expected: True")
+print(str(17), ps(s17), " - Expected: True")
+print(str(18), ps(s18), " - Expected: True")
+print(str(19), ps(s19), " - Expected: False")
+print(str(20), ps(s20), " - Expected: True")
+print(str(21), ps(s21), " - Expected: True")
+print(str(22), ps(s22), " - Expected: False")
+print(23, ps(s23), " - Expected: True")
+print(24, ps(s24), " - Expected: True")
+print(25, ps(s25), " - Expected: True")
+print(26, ps(s26), " - Expected: True")
+print(27, ps(s27), " - Expected: True")
+print(28, ps(s28), " - Expected: True")
+print(29, ps(s29), " - Expected: True")
+print(30, ps(s30), " - Expected: False")
